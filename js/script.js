95f86d2492b51a94565de8a5538d9802157b24f0
james_k <jamesk91@live.co.uk>
2023-05-01 21:29:28 +0100

    Created array, function with loop, and added an click event

--- js/script.js
+++ js/script.js
@@ -35,3 +35,29 @@ clover.sleep();
 baxter.play();
 
 console.log(clover, baxter);
+
+clover.isTired = 8;
+francine.isTired = 9;
+
+const allPets = [sora, clover, baxter, cleo, francine];
+
+console.log(allPets);
+
+const showPets = function (petArray) {
+    pets.innerHTML = "";
+
+    for (let pet of petArray) {
+        let status = "ready to play!";
+        if (pet.isTired >= 7) {
+            status = "sleeping";
+        }
+        let li = document.createElement("li");
+        li.innerHTML = `<span class="pet-name">${pet.name}</span> the ${pet.species} is ${status}.`;
+        pets.append(li);
+    }
+};
+
+statusButton.addEventListener("click", function () {
+    showPets(allPets);
+}
+);
